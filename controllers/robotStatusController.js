ec62944109d90fd0506fe7540a3e73317f444881
ankittchauhann <ankitt335407@gmail.com>
2024-01-25 13:49:31 +0530

    create robot added

--- controllers/robotStatusController.js
+++ controllers/robotStatusController.js
@@ -3,6 +3,17 @@ const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 
+const createRobot = async (req, res) => {
+  const error = robotStatusValidator(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const duplication = await RobotStatus.findOne({});
+  if (duplication) return res.status(400).send("Robot already exists");
+
+  const robot = new RobotStatus(_.pick(req.body, ["status"]));
+  await robot.save();
+  res.send(robot);
+};
 const updateRobotStatus = async (req, res) => {
   const error = robotStatusValidator(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -17,6 +28,13 @@ const updateRobotStatus = async (req, res) => {
   res.send(robotStatus);
 };
 
+const getRobots = async (req, res) => {
+  const robots = await RobotStatus.find();
+  res.send(robots);
+};
+
 module.exports = {
   updateRobotStatus,
+  createRobot,
+  getRobots,
 };
--- routes/robotStatus.js
+++ routes/robotStatus.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { updateRobotStatus } = require("../controllers/robotStatusController");
+const {
+  updateRobotStatus,
+  createRobot,
+  getRobots,
+} = require("../controllers/robotStatusController");
 const router = express.Router();
 
+router.route("/").post(createRobot).get(getRobots);
 router.route("/status").put(updateRobotStatus);
 
 module.exports = router;
